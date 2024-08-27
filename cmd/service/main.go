ef2a5c0c20e3ea809706d8d1631daf64eff1c572
Evan Ristow <evan.ristow1@gmail.com>
2024-08-27 12:16:58 -0400

    Add CORS allowed-methods. Add handler for CORS pre-flight OPTIONS request.

--- cmd/service/main.go
+++ cmd/service/main.go
@@ -26,16 +26,24 @@ func main() {
 	rootH := rest.NewRootHandler()
 	recipeH := rest.NewRecipeHandler(ds)
 
-	// Add test recipe
+	// Add test recipes
 	pancakeRecipe := recipe.NewRecipe(
 		"Pancakes",
 		[]string{"Flour", "Eggs", "Milk", "Sugar"},
 		[]string{"Mix ingredients", "Cook on pan"},
 	)
+	pizzaRecipe := recipe.NewRecipe(
+		"Pizza",
+		[]string{"Flour", "Yeast", "Sugar", "Olive Oil", "Salt", "Tomato Sauce", "Mozzarella Cheese", "Toppings"},
+		[]string{"Mix warm water, yeast, sugar. Cover and allow to rest for 5 min.", "Add flour, salt, olive oil. Mix until dough forms.", "Knead dough for 5 min.", "Cover and allow to rise for 1 hour.", "Preheat oven to 475F.", "Roll out dough.", "Add sauce, cheese, toppings.", "Bake for 13-15 min."},
+	)
+
 	ds.AddRecipe(pancakeRecipe.Id.String(), pancakeRecipe)
-	mux.Handle("/", rootH)
-	mux.Handle("/recipes/", recipeH)
-	mux.Handle("/recipes", recipeH)
+	ds.AddRecipe(pizzaRecipe.Id.String(), pizzaRecipe)
+
+	mux.Handle("/", rest.HandleCors(rootH))
+	mux.Handle("/recipes/", rest.HandleCors(recipeH))
+	mux.Handle("/recipes", rest.HandleCors(recipeH))
 	fmt.Println("Server running on port 8080")
 	log.Fatal(http.ListenAndServe(":8080", &slashFix{mux}))
 }
--- internal/rest/rest.go
+++ internal/rest/rest.go
@@ -23,6 +23,19 @@ type RecipeHandler struct {
 
 func enableCors(w *http.ResponseWriter) {
 	(*w).Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+	(*w).Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS")
+	(*w).Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type, X-Auth-Token")
+}
+
+func HandleCors(h http.Handler) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == "OPTIONS" {
+			enableCors(&w)
+			w.WriteHeader(http.StatusOK)
+		} else {
+			h.ServeHTTP(w, r)
+		}
+	}
 }
 
 func NewRootHandler() *RootHandler {
